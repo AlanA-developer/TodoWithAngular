bc451576de0418f19ee5d9e3e47bc47990e53ebb
Alan Alexis <alanalexisceb@hotmail.com>
2021-07-03 19:16:00 -0500

    Agregando todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { JumbotronComponent } from './jumbotron/jumbotron.component';
-import { TareaComponent } from './tarea/tarea.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     JumbotronComponent,
-    TareaComponent
   ],
   imports: [
     BrowserModule,
